40f18b662e54fa8d19aaab676c56125c433778ff
reus <reusee@gmail.com>
2014-03-10 19:34:05 +0800

    initial commit

---
+++ vpn.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+  "encoding/base64"
+  "fmt"
+  "io"
+  "io/ioutil"
+  "log"
+  "os"
+  "os/exec"
+  "strconv"
+  "strings"
+)
+
+func main() {
+  if len(os.Args) < 2 {
+    fmt.Printf("usage: %s [csv file]\n", os.Args[0])
+    return
+  }
+  content, err := ioutil.ReadFile(os.Args[1])
+  if err != nil {
+    log.Fatal(err)
+  }
+  n := 0
+  configs := make([]string, 0)
+  for _, line := range strings.Split(string(content), "\n") {
+    if len(line) == 0 || line[0] == '*' || line[0] == '#' {
+      continue
+    }
+    fields := strings.Split(line, ",")
+    speed, _ := strconv.Atoi(fields[4])
+    if speed < 30000000 {
+      continue
+    }
+    users, _ := strconv.Atoi(fields[9])
+    if users < 5000 {
+      continue
+    }
+    traffic, _ := strconv.Atoi(fields[10])
+    if traffic < 100000000000 {
+      continue
+    }
+    country := fields[6]
+    sessions, _ := strconv.Atoi(fields[7])
+    fmt.Printf("%2d %s %2d sessions, %5dG traffic, %6d users, %2dM speed\n", n, country, sessions, traffic/1000000000, users, speed/1000000)
+    n++
+    config := fields[14]
+    configs = append(configs, config)
+  }
+  print("which?\n")
+  fmt.Scanf("%d\n", &n)
+  data, err := base64.StdEncoding.DecodeString(configs[n])
+  if err != nil {
+    log.Fatal(err)
+  }
+  tmpFile, err := ioutil.TempFile("", "")
+  if err != nil {
+    log.Fatal(err)
+  }
+  tmpFile.Write(data)
+  tmpFile.Close()
+  cmd := exec.Command("/usr/bin/env", "sudo", "openvpn", "--config", tmpFile.Name())
+  stdout, err := cmd.StdoutPipe()
+  if err != nil {
+    log.Fatal(err)
+  }
+  go io.Copy(os.Stdout, stdout)
+  cmd.Run()
+}
